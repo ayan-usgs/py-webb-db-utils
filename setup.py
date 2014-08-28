2156a14b697e751eac486b41e7f204a817bb7499
Yan, Andrew N. <ayan@usgs.gov>
2014-08-28 11:58:42 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name='WEBB_DB_UTILS',
+    version='0.1.0',
+    author='Andrew Yan',
+    author_email='ayan@usgs.gov',
+    packages=['webb_utils'],
+    license='LICENSE.txt',
+    description='Python package for create dataframes and exports from the WEBB Database',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "SQLAlchemy == 0.9.7",
+        "cx-Oracle == 5.1.3",
+        "numpy == 1.8.1",
+        "pandas == 0.14.1",
+        "python-dateutil == 2.2",
+        "pytz == 2014.4",
+        "six == 1.7.3",
+    ],
+)
