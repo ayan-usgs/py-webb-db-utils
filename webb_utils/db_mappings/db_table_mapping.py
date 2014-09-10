c51e69a0e536ad9bff1db695d77c039a5ad887ac
Yan, Andrew N. <ayan@usgs.gov>
2014-09-10 11:30:59 -0500

    added all columns that get populated from a lab file upload, started database mappings.

--- webb_utils/db_mappings/db_table_mapping.py
+++ webb_utils/db_mappings/db_table_mapping.py
@@ -3,22 +3,35 @@ Created on Sep 9, 2014
 
 @author: ayan
 '''
-from sqlalchemy import Table, MetaData
+from sqlalchemy import Column, Integer, String, Numeric, DateTime, ForeignKey, Float
 from sqlalchemy.ext.declarative import declarative_base
-from upload_columns import ANION_COLUMNS
-from ..db_utils import AlchemDB
 
 
-meta = MetaData()
 Base = declarative_base()
-
-
-anion = Table('ANION', meta, autoload=True)
 
 
 class Anion(Base):
     
-    __table__ = anion
-    __mapper_args__ = {
-                       'include_properties': list(ANION_COLUMNS)
-                       }
+    __table__ = 'ANION'
+    
+    record_number = Column(Float, asdecimal=False, primary_key=True, nullable=False, ForeignKey(u'sample.record_number'))
+    analyzing_lab = Column(String, primary_key=True, ForeignKey(u'lab.analyzing_lab'), nullable=False)
+    cl = Column(Float, asdecimal=False)
+    no3 = Column(Float, asdecimal=False)
+    so4 = Column(Float, asdecimal=False)
+    flagcl = Column(String)
+    flagno3 = Column(String)
+    flagso4 = Column(String)
+    alkalinity_source = Column(String)
+    alkalinity = Column(Float)
+    
+
+class Cation(Base):
+    
+    __table__ = 'CATION'
+    
+    record_number = Column(Float, asdecimal=False, nullable=False, ForeignKey(u'sample.record_number'))
+    analyzing_lab = Column(String, primary_key=True, ForeignKey(u'lab.analyzing_lab'), nullable=False)
+    na = Column(Float, asdecimal=False)
+    mg = Column(Float, asdecimal=False)
+    
